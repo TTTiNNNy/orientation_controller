d3e6cc0b5d9a8a5920f95a30598b72a2e6aec4cc
Тыманович Никита Александрович <tymanovich_na@peleng.jsc.local>
2024-05-07 17:55:38 +0300

    fix implementation

--- src/services/esc/dshot.rs
+++ src/services/esc/dshot.rs
@@ -1,38 +1,37 @@
 use core::{borrow::Borrow, marker::ConstParamTy};
-use embassy_nrf::gpio::{AnyPin, Output, Pin};
 use embassy_time::Timer;
 
 use crate::services::esc::api::EscApi;
 
 #[derive(ConstParamTy, PartialEq, Eq)]
-enum DShotTime{
-    t150 = 1,
-    t300,
-    t600,
-    t1200
+enum Freq{
+    d150 = 1,
+    d300,
+    d600,
+    d1200
 }
 
-struct DShot <const time: DShotTime>{
-    output: Output<'static, AnyPin>
+struct DShot <const time: Freq, T: embedded_hal::digital::OutputPin>{
+    output: T
 }
 
     fn crc(payload: &u16) -> u8 { 
        ((*payload ^ (*payload >> 4) ^ (*payload >> 8)) & 0x0F) as u8
     }
 
-    impl <const TIME: DShotTime> DShot<TIME>{
+    impl <const TIME: Freq, T: embedded_hal::digital::OutputPin> DShot<TIME, T>{
         fn build_package(&self, payload: u16) -> u16 { payload << 5 + crc(&payload) }
     }
 
 
-impl <const TIME: DShotTime> EscApi for DShot<TIME>{
+impl <const TIME: Freq, T:  embedded_hal::digital::OutputPin> EscApi for DShot<TIME, T>{
     async fn set_power(&mut self, power_percent: u8){
         const MAX_GRAD_SIZE: u16 = 2000;
         const BIT_NS_BASE_PERIOD_TIME: u16 = 6670;
         const BIT_NS_BASE_RAISE_TIME: u16 = 5000;
 
-        let BIT_PERIOD = BIT_NS_BASE_PERIOD_TIME / TIME as u16;
-        let RAISE_PERIOD = BIT_NS_BASE_PERIOD_TIME / TIME as u16;
+        let bit_period = BIT_NS_BASE_PERIOD_TIME / TIME as u16;
+        let raise_period = BIT_NS_BASE_RAISE_TIME / TIME as u16;
 
         let msb = 1 << (u16::MAX - 1);
         
@@ -43,14 +42,14 @@ impl <const TIME: DShotTime> EscApi for DShot<TIME>{
             self.output.set_high();
             let sleep_period;
             if package & msb != 0{ 
-                sleep_period = RAISE_PERIOD as u64;
-                Timer::after_nanos(RAISE_PERIOD as u64).await;
+                sleep_period = raise_period as u64;
+                Timer::after_nanos(raise_period as u64).await;
             } else {
-                sleep_period = RAISE_PERIOD as u64 / 2;
-                Timer::after_nanos(RAISE_PERIOD as u64 / 2).await;
+                sleep_period = raise_period as u64 / 2;
+                Timer::after_nanos(raise_period as u64 / 2).await;
             }
             self.output.set_low();
-            Timer::after_nanos(BIT_PERIOD as u64 - sleep_period).await;
+            Timer::after_nanos(bit_period as u64 - sleep_period).await;
 
             package <<= 1;
         }
