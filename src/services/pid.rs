c0cdb02020bc20cc5202350b854f2e964d0b061f
Тыманович Никита Александрович <tymanovich_na@peleng.jsc.local>
2024-05-10 21:26:51 +0300

    add basic pid template

--- src/services/pid.rs
+++ src/services/pid.rs
@@ -1,19 +1,58 @@
+use core::marker::ConstParamTy;
+
 use crate::services::esc::api::EscApi;
-use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Receiver};
+use embassy_sync::{
+    blocking_mutex::raw::NoopRawMutex,
+    channel::{Receiver, Sender},
+};
 use nalgebra::Vector3;
 use pid::Pid;
 
-#[embassy_executor::task]
-pub async fn pid(
+#[derive(ConstParamTy, PartialEq, Eq)]
+
+enum QuadroRotors {
+    Xpyp,
+    Xpyn,
+    Xnyn,
+    Xnyp,
+}
+
+enum CopterType {
+    Three,
+    Quadro(QuadroRotors),
+    Octa,
+    Hex,
+}
+
+async fn quadro_pid_calc(
+    pid: Pid<f32>,
+    motor_type: QuadroRotors,
     mut esc: impl EscApi + 'static,
     control_degree_receiver: Receiver<'static, NoopRawMutex, Vector3<f32>, 1>,
-    xpyp: Sender<'static, NoopRawMutex, LedState, 1>,
-    xpyn: Sender<'static, NoopRawMutex, LedState, 1>,
-    xnyn: Sender<'static, NoopRawMutex, LedState, 1>,
-    xnyp: Sender<'static, NoopRawMutex, LedState, 1>,
+    orientation_receiver: Receiver<'static, NoopRawMutex, Vector3<f32>, 1>,
+    xxyy: Sender<'static, NoopRawMutex, u8, 1>,
+) {
+    loop {
+        0
+
+    }
+}
+
+#[embassy_executor::task]
+pub async fn pid(
+    deviceType: CopterType,
+    mut esc: impl EscApi + 'static,
+    control_degree_receiver: Receiver<'static, NoopRawMutex, Vector3<f32>, 1>,
+    orientation_receiver: Receiver<'static, NoopRawMutex, Vector3<f32>, 1>,
+    xxyy: Sender<'static, NoopRawMutex, u8, 1>,
 ) {
     let mut pid: Pid<f32> = Pid::new(0.0, 3600.0);
 
-    
-    loop {}
-}
+    match deviceType {
+    CopterType::Quadro(motor_type) => quadro_pid_calc(pid, motor_type, esc, control_degree_receiver, orientation_receiver, xxyy).await,
+    _ => todo!()
+    }
+        
+    }
+
+
